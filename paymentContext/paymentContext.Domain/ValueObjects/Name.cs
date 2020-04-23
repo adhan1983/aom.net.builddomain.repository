5c0ea9db2156113aefcccbdf14718636b16a1bd7
Adhan de Oliveira Maldonado <3843070+adhan1983@users.noreply.github.com>
2020-04-22 22:39:18 -0300

    The Notification validation has been added in the Project

--- paymentContext/paymentContext.Domain/ValueObjects/Name.cs
+++ paymentContext/paymentContext.Domain/ValueObjects/Name.cs
@@ -4,6 +4,15 @@ namespace PaymentContext.Domain.ValueObjects
 {
     public class Name : ValueObject
     {
+        public Name(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+
+            if (!string.IsNullOrWhiteSpace(FirstName))
+                AddNotification("Name.FirstName", "Invalid name");
+        }
+
         public string FirstName { get; private set; }
         public string LastName { get; private set; }
     }
